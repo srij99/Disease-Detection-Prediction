51f57ce462168d3d416f980445a24ffeb3f180cb
Sreejith Sathyadevan <87577589+srij99@users.noreply.github.com>
2022-09-14 19:53:30 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,12 +6,6 @@ email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
-[theme]
-primaryColor = ‘#CE00FF’
-backgroundColor = ‘#000000’
-secondaryBackgroundColor = ‘#420056’
-textColor= ‘#FFFFFF’
-font = ‘sans serif’
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
